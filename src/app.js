565f458dfd3cb0f2f89d79f8819b70d73342d2e4
victorluadev <streamyodin@gmail.com>
2022-12-05 22:49:30 -0300

    feat: Rotas de PUT e GET

--- src/app.js
+++ src/app.js
@@ -16,9 +16,26 @@ app.get('/books', (req, res) => {
   res.status(200).json(books);
 });
 
+app.get('/books/:id', (req, res) => {
+  let index = findBook(req.params.id);
+
+  res.json(books[index]);
+});
+
 app.post('/books', (req, res) => {
   books.push(req.body);
   res.status(201).send('Created');
 });
+
+app.put('/books/:id', (req, res) => {
+  let index = findBook(req.params.id);
+  books[index].title = req.body.title; 
+
+  res.json(books);
+});
+
+function findBook(id) {
+  return books.findIndex( book => book.id == id);
+}
 
 export default app;
